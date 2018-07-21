dcfa2bae8d94e3787f5a8e41a5dc80ddfb70d554
erikvw <ew2789@gmail.com>
2018-07-21 08:15:48 +0200

    setup files, fix test

--- edc_navbar/tests/test_navbar.py
+++ edc_navbar/tests/test_navbar.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, tag
 from django.urls.base import reverse
+from django.urls.exceptions import NoReverseMatch
 
 from ..navbar import Navbar
 from ..navbar_item import NavbarItem, NavbarItemError
@@ -49,7 +50,7 @@ class TestNavbar(TestCase):
 
     def test_navbar_item_raises_bad_url(self):
         self.assertRaises(
-            NavbarItemError,
+            NoReverseMatch,
             NavbarItem,
             name='navbar_item_one',
             label='Navbar Item One',
--- edc_navbar/urls.py
+++ edc_navbar/urls.py
@@ -11,7 +11,7 @@ if settings.APP_NAME == 'edc_navbar':
     urlpatterns = [
         path(r'one/', View.as_view(), name='navbar_one_url'),
         path(r'two/', View.as_view(), name='navbar_two_url'),
-        path(r'logout', View.as_view(), name='logout_url'),
+        path(r'logout', View.as_view(), name='logout'),
         path(r'administration', View.as_view(), name='administration_url'),
         path(r'', View.as_view(), name='home_url'),
     ]
