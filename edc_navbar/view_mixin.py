4aa852acf03c68252030fcdb63477c80229cac09
erikvw <ew2789@gmail.com>
2024-01-28 08:25:29 -0600

    call view attr 'navbar_selected_item' via method that can be overridden

--- edc_navbar/view_mixin.py
+++ edc_navbar/view_mixin.py
@@ -24,7 +24,7 @@ class NavbarViewMixin:
 
     def get_navbar_context_data(self, context) -> dict:
         navbar = site_navbars.get_navbar(name=self.get_navbar_name())
-        navbar.set_active(self.navbar_selected_item)
+        navbar.set_active(self.get_navbar_selected(**context))
         context.update(navbar=navbar)
         app_config = django_apps.get_app_config("edc_navbar")
         default_navbar_name = app_config.default_navbar_name
@@ -35,3 +35,6 @@ class NavbarViewMixin:
                 default_navbar=default_navbar, default_navbar_name=default_navbar_name
             )
         return context
+
+    def get_navbar_selected(self, **kwargs) -> str:
+        return self.navbar_selected_item
